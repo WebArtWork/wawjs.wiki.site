1e7025526b39b76d031df197945f7d9277fad739
Nika <danzi6386@gmail.com>
2025-07-11 14:13:25 +0300

    cleanup code

--- js/index.js
+++ js/index.js
@@ -1,21 +1,21 @@
 $(document).ready(function () {
-  $(".owl-carousel").owlCarousel({
-    loop: true,
-    nav: true,
-    dots: false,
-    navText: [
-      '<img src="/img/arrow-prev.svg" alt="previous" class="support__prev-item"></img>',
-      '<img src="/img/arrow-next.svg" alt="next" class="support__next-item">',
-    ],
-  });
+	$(".owl-carousel").owlCarousel({
+		loop: true,
+		nav: true,
+		dots: false,
+		navText: [
+			'<img src="img/arrow-prev.svg" alt="previous" class="support__prev-item"></img>',
+			'<img src="img/arrow-next.svg" alt="next" class="support__next-item">',
+		],
+	});
 });
 
 $("#toogleNavs").on("click", function () {
-  $(this).toggleClass("_active");
-  $("#landNavs").toggleClass("_active");
+	$(this).toggleClass("_active");
+	$("#landNavs").toggleClass("_active");
 });
 
 $(".jsNavElement").on("click", function () {
-  $("#toogleNavs").removeClass("_active");
-  $("#landNavs").removeClass("_active");
+	$("#toogleNavs").removeClass("_active");
+	$("#landNavs").removeClass("_active");
 });
